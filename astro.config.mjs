8d96bdcc7b37376fa106586f1c543d220897f0eb
cjapar2 <cjapar2@gmail.com>
2025-08-17 21:25:44 -0700

    Adding changes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const SERVER_PORT = 4321;
+
+const LOCALHOST_URL = `http://localhost:4321/`;
+
+const LIVE_URL = 'https://cjapar2.github.io';
+
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+if (isBuild) {
+    BASE_URL = LIVE_URL;
+}
+console.log(BASE_URL);
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cjapar2.github.io',
-    base: '/portfolio-astro',
 });
